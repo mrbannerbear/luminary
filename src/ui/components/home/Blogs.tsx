7ba3de6726f22d2eb24aac86ac73cf473ce65fb3
S M Mishkatus Saqlain <smmsaqlain@gmail.com>
2024-04-08 03:34:46 +0600

    Added blog section on homepage

--- src/ui/components/home/Blogs.tsx
+++ src/ui/components/home/Blogs.tsx
@@ -1,5 +1,6 @@
 import Section from "@/ui/shared/Section";
 import sampleBlogs from "../../../../public/blogs.json";
+import Link from "next/link";
 
 const Blogs = () => {
   interface cardObjType {
@@ -16,21 +17,29 @@ const Blogs = () => {
       <div>
         <h2>From the Journal</h2>
       </div>
-      <div className="grid grid-cols-3 gap-5">
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-5">
         {sampleBlogs.slice(0, 3).map((each: cardObjType) => (
           <div
             key={each.id}
-            className="w-full bg-purple-50 overflow-hidden h-full border border-purple-950"
+            className="w-full bg-purple-50 border border-purple-950 overflow-hidden"
           >
-            <div className="px-4 py-3">
+            <div className="px-4 py-3 relative">
               <h3 className="text-lg font-semibold text-purple-950">
                 {each.title}
               </h3>
-              <p className="text-sm text-purple-950">{each.author}</p>
-              <p className="overflow-hidden text-ellipsis">{each.content}</p>
+              <p className="text-sm text-purple-950 my-2">{each.author}</p>
+              <p className="overflow-hidden text-ellipsis mb-2">{each.content.slice(0, 100)}...</p>
+              <Link href={`/blog/${each.id}`}><span className="underline">Read More</span></Link>
             </div>
           </div>
         ))}
+      </div>
+      <div className="flex justify-center mt-10">
+        <Link href={"/blog"}>
+        <button>
+            All Blogs
+        </button>
+        </Link>
       </div>
     </Section>
   );
